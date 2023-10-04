0d6bc33ed8c16e5cb8e28911fcc269e467dff6df
anggraenipsp <anggraenipuspa1234@gmail.com>
2023-10-05 00:21:39 +0700

    [Bootcamp08] delete

--- modules/bootcamp07/controllers/Bootcamp07.php
+++ modules/bootcamp07/controllers/Bootcamp07.php
@@ -6,14 +6,13 @@ class Bootcamp07 extends CI_Controller {
 	function __construct() 
 	{
 		parent::__construct($securePage=false);
-		$this->load->model('bootcamp07_model');
+		$this->load->model('Bootcamp07_model');
 	}
 
 	public function index()
 	{
 		$data['user']=$this->input->get_post('id');
-		$this->load->model('bootcamp07_model');
-		$jabatan_options = $this->bootcamp07_model->getJabatanOptions();
+		$jabatan_options = $this->Bootcamp07_model->getJabatanOptions();
 		$data['jabatan_options'] = $jabatan_options;
 
 		$this->load->view('bootcamp07_view',$data);
@@ -21,37 +20,34 @@ class Bootcamp07 extends CI_Controller {
 	}
 
 	public function getListData(){
-		$data = $this->bootcamp07_model->getListData();
+		$data = $this->Bootcamp07_model->getListData();
 		echo $data;
 	}
 	
 	public function addData(){
     $nik = $this->input->post('nik');
 
-    if ($this->bootcamp07_model->isNikExists($nik)) {
+    if ($this->Bootcamp07_model->isNikExists($nik)) {
         echo "<script>alert('NIK sudah digunakan. Harap masukkan NIK yang baru');</script>";
 		echo "<script>window.history.back();</script>";
     } else {
-        $this->bootcamp07_model->save();
+        $this->Bootcamp07_model->save();
         redirect('bootcamp07/?id=anggi');
     }
 	
 	}
 		
-	public function deleteData($nik){
+	public function deleteData(){
 		$nik = $this->input->post('nik');
-		// Menghapus data dari database
-		$this->load->model('Bootcamp07_model');
 		$this->Bootcamp07_model->deleteData($nik);
-		// Mengirim respons ke klien
 		$this->output->set_content_type('application/json');
 		$this->output->set_status_header(200);
 		$this->output->set_output(json_encode(['message' => 'Data berhasil dihapus']));
 	}
 		
-	function editData($nik){
+	function editData(){
 		$where = array('nik' => $nik);
-		$data['karyawan'] = $this->bootcamp07_model->editData($where,'karyawan')->result();
+		$data['karyawan'] = $this->Bootcamp07_model->editData($where,'karyawan')->result();
 		$this->load->view('bootcamp07_view_edit',$data);
 	}
 	
